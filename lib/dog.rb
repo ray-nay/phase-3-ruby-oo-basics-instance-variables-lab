8fa26250095dc9ff24835aeeb032d8f067cbb325
Renee Mumbi <1723renee@gmail.com>
2023-02-09 16:42:56 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
 
-end
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+  
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  
+  puts lassie.name
+
+  mike=Dog.new
+  mike.name="mike"
+  puts mike.name
